667bb7e47d163b2a0026a6fe108d2525853e30d6
Xargin <cao1988228@163.com>
2019-12-10 00:19:05 +0800

    basic layout

--- wrkrs/src/main.rs
+++ wrkrs/src/main.rs
@@ -2,7 +2,8 @@ use futures::executor::LocalPool;
 use futures::task::SpawnExt;
 use async_std::net::{TcpStream};
 use std::net::SocketAddr;
-use async_std::io;//::{self};
+use async_std::io;
+use async_std::prelude::*;
 
 async fn connect(addr: &SocketAddr) -> io::Result<TcpStream> {
     match TcpStream::connect(addr).await {
@@ -22,20 +23,32 @@ async fn connect(addr: &SocketAddr) -> io::Result<TcpStream> {
 fn main() {
     let thread_num = 10;
     let connection_per_thread = 10;
-    let addr = "localhost:9999";
+    let addr = "localhost:9999".parse().unwrap();
 
     let mut thread_handle_list= vec![];
     (0..thread_num).for_each(|_|{
         let h = std::thread::spawn(move || {
             let mut pool = LocalPool::new();
-            let mut spawner = pool.spawner();
+            let spawner = pool.spawner();
             (0..connection_per_thread).for_each(|i|{
                 println!("{}", i);
                 spawner.spawn(async move {
+                    let mut read_buffer = [0u8; 1024];
+                    let mut stream = connect(&addr).await.unwrap();
                     // load test task
-                    println!("{}", "internal");
-
-                });
+                    // write request text here
+                    match stream.write_all(b"abc").await {
+                        Ok(_) => {
+                            match stream.read(&mut read_buffer).await {
+                                Ok(_) => {},
+                                Err(_) => {},
+                            }
+                        },
+                        Err(e) => {
+                            println!("{}", e)
+                        },
+                    };
+                }).unwrap();
             });
             pool.run();
         });
